bae15dfcbbc643a933271802ef8b6789e5753bff
Abhishek Nagrecha, MSc <mailnagrecha@gmail.com>
2025-02-20 19:48:39 -0500

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -333,18 +333,4 @@
 
 
 })();
-// Animation on Scroll
-document.addEventListener("DOMContentLoaded", function() {
-    const timelineItems = document.querySelectorAll(".timeline-item");
 
-    function checkScroll() {
-        timelineItems.forEach((item) => {
-            if (item.getBoundingClientRect().top < window.innerHeight - 100) {
-                item.classList.add("show");
-            }
-        });
-    }
-
-    window.addEventListener("scroll", checkScroll);
-    checkScroll();
-});
